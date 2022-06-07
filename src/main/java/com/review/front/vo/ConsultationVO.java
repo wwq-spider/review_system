d812706108896146981fad89827708c56fb48b3e
heyq <854180667@qq.com>
2022-06-07 19:54:06 +0800

    专家排班

--- src/main/java/com/review/front/vo/ConsultationVO.java
+++ src/main/java/com/review/front/vo/ConsultationVO.java
@@ -7,75 +7,79 @@ import java.io.Serializable;
  * @date 2022/6/2
  */
 public class ConsultationVO implements Serializable {
-    /*
+    /**
         预约id
      */
     private Integer id;
-    /*
+    /**
         日历id
      */
     private Integer calendarId;
-    /*
+    /**
         用户id
      */
     private String userId;
-    /*
+    /**
         专家姓名
      */
     private String expertName;
-    /*
+    /**
         专家性别
      */
     private Integer expertSex;
-    /*
+    /**
         专家职称
      */
     private String expertJobTitle;
-    /*
+    /**
         专家介绍
      */
     private String expertIntroduction;
-    /*
+    /**
         专家标签
      */
     private String expertLable;
-    /*
+    /**
+     * 专家电话
+     */
+    private String expertPhone;
+    /**
         预约人姓名
      */
     private String patientName;
-    /*
+    /**
         预约人性别
      */
     private String patientSex;
-    /*
+    /**
         预约人年龄
      */
     private Integer patientAge;
-    /*
+    /**
         预约人电话
      */
     private String userPhone;
-    /*
+    /**
         预约人身份证号
      */
     private String userIdCard;
-    /*
+    /**
         预约开始时间
      */
     private String beginTime;
-    /*
+    /**
         预约结束时间
      */
     private String endTime;
-    /*
+    /**
         周几（1，2，3...）
      */
     private Integer weekDay;
-    /*
+    /**
         周几（周一，周二，周三...）
      */
     private String weekDayName;
-    /*
+    /**
         预约状态
      */
     private Integer status;
@@ -143,6 +147,14 @@ public class ConsultationVO implements Serializable {
 
     public void setExpertLable(String expertLable) {
         this.expertLable = expertLable;
+    }
+
+    public String getExpertPhone() {
+        return expertPhone;
+    }
+
+    public void setExpertPhone(String expertPhone) {
+        this.expertPhone = expertPhone;
     }
 
     public String getPatientName() {
